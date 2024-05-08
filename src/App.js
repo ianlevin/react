ff8ae1f0ebcf9669a554407bf510edb41d8a5708
IAN GABRIEL LEVIN <47295402@ort>
2024-05-08 14:25:02 -0300

    a

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Boton from './components/Boton';
+
+function App() {
+  return (
+    <div>
+      <Boton texto = "gordo"/>
+      <Boton texto = "gey"/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Boton.jsx
@@ -0,0 +1,5 @@
+const Boton = (params) =>{
+    return <button onClick={() => alert(`yuco ${params.texto}`)}>yuco?</button>
+}
+
+export default Boton;
