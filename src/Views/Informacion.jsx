c067fc6d281daf947fdb155bc16f343b041b735a
IAN GABRIEL LEVIN <47295402@ort>
2024-05-15 14:30:34 -0300

    b

--- src/Views/Informacion.jsx
+++ src/Views/Informacion.jsx
@@ -1,7 +1,15 @@
+import fotoian from '../img/fotoian.png'
+
 const Informacion = (params) =>{
 return(
-    <div>
+    <div className="informacion">
         <h1>Hola! Me llamo Ian Levin</h1>
+        <p>Soy Licenciado en Sistemas recibido en la Universidad de Palermo. 
+            Además, hace más de 9 años que trabajo en educación IT, 
+            transmitiendo los conceptos de programación a cientos de 
+            jóvenes de nivel secundario y terciario.</p>
+
+            <img src={fotoian} alt="fotoian" />
     </div>
 )
 }
